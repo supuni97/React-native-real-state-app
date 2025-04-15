925081daf6b4e2c47ce377110adb86f205a1150d
supuni97 <sitharabandara1997@gmail.com>
2025-04-15 11:02:56 +0530

    Added logout function

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -56,6 +56,7 @@ const Profile = () => {
 
     if (result) {
       Alert.alert("Success, You have been logged out successfully");
+      refetch();
     } else {
       Alert.alert("Error, An error occurred while logging out");
     }
